4a77b7036eb0784889ebd9e01b3f3748087f22c7
Christuss <szendefi.krisztofer@gmail.com>
2023-09-06 13:19:03 +0200

    elso

---
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -0,0 +1,30 @@
+
+package gondolatolvaso;
+
+import java.util.Scanner;
+
+
+public class Gondolatolvaso {
+
+    public static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args) {
+        
+    }
+    
+    public void kirak(String[] kartyak) {
+        for (String kartya : kartyak) {
+            System.out.print(kartya + " ");
+        }
+        System.out.println();
+    }
+    public void melyik() {
+        
+    }
+    public void kever() {
+        
+    }
+    public void ezVolt() {
+        
+    }
+    
+}
