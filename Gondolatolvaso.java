1824e0f432818282ba49364bbbd97778f8aa6dc5
Christuss <115688591+Christuss@users.noreply.github.com>
2023-09-06 13:48:23 +0200

    kirak tartalma javitas

--- Gondolatolvaso.java
+++ Gondolatolvaso.java
@@ -1,31 +1,47 @@
-
 package gondolatolvaso;
 
 import java.util.Scanner;
 
-
 public class Gondolatolvaso {
 
-    public static Scanner sc = new Scanner(System.in); 
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
     }
-    
-    public void kirak(String[] kartyak) {
-        for (String kartya : kartyak) {
-            System.out.print(kartya + " ");
+
+    public void kirak() {
+        String[] pakli = new String[22];
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        for (int i = 0; i < pakli.length; i++) {
+            for (String szin : szinek) {
+                if (szin != "M") {
+                    for (String ertek : ertekek) {
+                        pakli[i] = szin + ertek;
+                    }
+                }else{
+                    for (int j = 0; j < 3; j++) {
+                        pakli[i] = szin + ertekek[j];
+                    }
+                }
+
+            }
+
         }
-        System.out.println();
     }
+
     public int melyik() {
         int melyikOszlop = sc.nextInt();
         return melyikOszlop;
     }
+
     public void kever() {
-        
+
     }
+
     public void ezVolt(String kartya) {
         System.out.println(kartya);
     }
-    
+
 }
