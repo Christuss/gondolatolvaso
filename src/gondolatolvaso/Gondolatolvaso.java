ce840fc5c1b654b6722fa6d0121464c541564834
KSZBence <kunszekelybence@gmail.com>
2023-09-11 14:41:46 +0200

    kever metódus swich case 1

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -44,8 +44,22 @@ public class Gondolatolvaso {
         } while (!jo);
     }
 
-    public void kever() {
-
+    public void kever(String[] pakli, int valsztott) {
+        switch (valsztott) {
+            case 1:
+                for (int i = 0; i < 7; i++) {
+                    pakli[i] = pakli[20-(i-1)*3];
+                    pakli[i+7] = pakli[19-(i-1)*3];
+                    pakli[i+14] = pakli[21-(i-1)*3];
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+           for (int i = 0; i < pakli.length; i++) {
+               System.out.println(pakli[i]);
+        }
     }
 
 
