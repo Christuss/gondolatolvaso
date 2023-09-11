530d1cd0cb30c19d3cc5e85d1d923ddf058f1e49
Christuss <szendefi.krisztofer@gmail.com>
2023-09-11 13:22:07 +0200

    melyik metódus

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -1,31 +1,44 @@
-
 package gondolatolvaso;
 
 import java.util.Scanner;
 
-
 public class Gondolatolvaso {
 
-    public static Scanner sc = new Scanner(System.in); 
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        new gondolatolvaso.Gondolatolvaso().kirak();
+    }
+
+    public void kirak() {
+        String[] pakli = new String[22];
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < pakli.length; i++) {
+                if (i % 3 == 0) {
+                    System.out.println();
+                }
+                System.out.print(pakli[i] + "  ");
+            }
+            int melyikOszlop = melyik();
+        }
         
     }
-    
-    public void kirak(String[] kartyak) {
-        for (String kartya : kartyak) {
-            System.out.print(kartya + " ");
-        }
-        System.out.println();
-    }
+
     public int melyik() {
+        System.out.print("Melyik oszlop (1-3): ");
         int melyikOszlop = sc.nextInt();
         return melyikOszlop;
     }
+
     public void kever() {
-        
+
     }
+
     public void ezVolt(String kartya) {
         System.out.println(kartya);
     }
-    
+
 }
