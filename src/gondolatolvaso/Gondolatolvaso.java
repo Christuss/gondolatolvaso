63273dd63f2caaeab338ca52f452a8b038638fe3
KSZBence <kunszekelybence@gmail.com>
2023-09-26 10:24:55 +0200

    kirak javitasa, feltolt, kever változtatva, késznek tűnik

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class Gondolatolvaso {
 
     String[] pakli = new String[22];
-
+    
     public static Scanner sc = new Scanner(System.in);
-
+    
     public static void main(String[] args) {
-        new gondolatolvaso.Gondolatolvaso().kirak();
+        Gondolatolvaso gondolatolvaso = new gondolatolvaso.Gondolatolvaso();
+        gondolatolvaso.feltolt();
+        for (int i = 0; i < 3; i++) {
+            gondolatolvaso.kirak();
+            gondolatolvaso.melyik();
+        }
+        gondolatolvaso.ezVolt();
     }
-
-    public void kirak() {
+    
+    public void feltolt(){
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
 
@@ -22,18 +28,17 @@ public class Gondolatolvaso {
                 pakli[i++] = szin + "_" + ertekek[e];
             }
         }
-
-        for (int j = 0; j < 3; j++) {
-            for (int f = 1; f < pakli.length; f++) {
-                if (f % 3 == 1) {
-                    System.out.println();
-                }
-                System.out.printf("%10s",pakli[f]);
+    }
+    
+    public void kirak() {
+                
+        for (int j = 1; j < 22; j++) {
+            System.out.printf("%10s",pakli[j]);
+            if (j % 3 == 0) {
+                System.out.println();
             }
-            System.out.println("");
-            melyik();
+            
         }
-
     }
 
     public void melyik() {
@@ -47,30 +52,34 @@ public class Gondolatolvaso {
     }
 
     public void kever(int oszlop) {
+        String[] ujpakli = new String[22];
         switch (oszlop) {
             case 1:
-                for (int i = 1; i < 7; i++) {
-                    pakli[i] = pakli[20 - (i - 1) * 3];
-                    pakli[i + 7] = pakli[19 - (i - 1) * 3];
-                    pakli[i + 14] = pakli[21 - (i - 1) * 3];
+                for (int i = 1; i <= 7; i++) {
+                    ujpakli[i] = pakli[20 - (i - 1) * 3];
+                    ujpakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujpakli[i + 14] = pakli[21 - (i - 1) * 3];
                 }
                 break;
             case 2:
-                for (int i = 1; i < 7; i++) {
-                    pakli[i] = pakli[19 - (i - 1) * 3];
-                    pakli[i + 7] = pakli[20 - (i - 1) * 3];
-                    pakli[i + 14] = pakli[21 - (i - 1) * 3];
+                for (int i = 1; i <= 7; i++) {
+                    ujpakli[i] = pakli[19 - (i - 1) * 3];
+                    ujpakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujpakli[i + 14] = pakli[21 - (i - 1) * 3];
                 }
                 break;
-
             case 3:
-                for (int i = 1; i < 7; i++) {
-                    pakli[i] = pakli[21 - (i - 1) * 3];
-                    pakli[i + 7] = pakli[20 - (i - 1) * 3];
-                    pakli[i + 14] = pakli[19 - (i - 1) * 3];
+                for (int i = 1; i <= 7; i++) {
+                    ujpakli[i] = pakli[19 - (i - 1) * 3];
+                    ujpakli[i + 7] = pakli[21 - (i - 1) * 3];
+                    ujpakli[i + 14] = pakli[20 - (i - 1) * 3];
                 }
                 break;
         }
-
+        pakli = ujpakli;
+    }
+    
+    public void ezVolt(){
+        System.out.println("A választott lap: %s".formatted(pakli[11]));
     }
 }
